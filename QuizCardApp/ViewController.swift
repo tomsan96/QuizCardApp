0ac0a6615d7392bf9a7a256531a6b74acf5c3aaf
tomsan96 <gradada0507@gmail.com>
2021-06-20 16:42:03 +0900

    初期プロジェクトの作成

---
+++ QuizCardApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizCardApp
+//
+//  Created by 山崎定知 on 2021/06/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
