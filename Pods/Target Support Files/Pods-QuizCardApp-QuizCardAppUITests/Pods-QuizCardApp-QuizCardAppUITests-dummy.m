bbfb0d1487830cd490936a772d01fc0a11b60a71
tomsan96 <gradada0507@gmail.com>
2021-06-20 17:04:31 +0900

    Podfileの追加

---
+++ Pods/Target Support Files/Pods-QuizCardApp-QuizCardAppUITests/Pods-QuizCardApp-QuizCardAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QuizCardApp_QuizCardAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_QuizCardApp_QuizCardAppUITests
+@end
---
+++ Pods/Target Support Files/Pods-QuizCardApp/Pods-QuizCardApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QuizCardApp : NSObject
+@end
+@implementation PodsDummy_Pods_QuizCardApp
+@end
